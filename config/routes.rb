a93411f28a3d16cd8212ea60c329780915b5c81a
bruno della mattia <brunodellamattia@gmail.com>
2018-05-15 15:10:20 +0200

    refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get    "tasks",          to: "tasks#index"
-  get    "tasks/new",      to: "tasks#new"
+  get    "tasks/new",      to: "tasks#new", as: "new"
   post   "tasks",          to: "tasks#create"
-  get    "tasks/:id",      to: "tasks#show"
-  get    "tasks/:id/edit", to: "tasks#edit"
+  get    "tasks/:id",      to: "tasks#show",   as: "task"
+  get    "tasks/:id/edit", to: "tasks#edit", as: "edit"
   patch  "tasks/:id",      to: "tasks#update"
   delete "tasks/:id",      to: "tasks#destroy"
-  end
-
+end
